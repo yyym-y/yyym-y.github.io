251d35588f172f249d3f93040be0bbd7d990abd1
yyym-y <yinmingyang89@gmail.com>
2024-04-16 17:25:13 +0800

    add two scatter note, fix click scatter note url bug

--- js/data/scatterData.js
+++ js/data/scatterData.js
@@ -1,5 +1,6 @@
 export default {
     blogs : [
-       
+        {name : "MyBatis工作原理", link : "/pdf/Scattered/MyBatis工作原理.pdf", time : "2024-04-06"},
+        {name : "MyBatis映射器概述", link : "/pdf/Scattered/MyBatis映射器概述.pdf", time : "2024-04-06"},
     ]
 }
