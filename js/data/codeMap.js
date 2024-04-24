508cf5249a30b4b96c9ee9acb5beaa417bf28339
yyym-y <yinmingyang89@gmail.com>
2024-04-24 09:52:53 +0800

    change codeShow form

---
+++ js/data/codeMap.js
@@ -0,0 +1,6 @@
+export default {
+    info : [
+        {name : "tcp_sender.cc", url : "/file/code/cpp/tcp_sender.cc"},
+        {name : "tcp_sender.hh", url : "/file/code/cpp/tcp_sender.hh"},
+    ]
+}
--- js/data/scatterData.js
+++ js/data/scatterData.js
@@ -3,6 +3,7 @@ export default {
         {name : "MyBatis工作原理", link : "/pdf/Scattered/MyBatis工作原理.pdf", time : "2024-04-06"},
         {name : "MyBatis映射器概述", link : "/pdf/Scattered/MyBatis映射器概述.pdf", time : "2024-04-06"},
         {name : "MySQL 快速手册", link : "/2023/12/30/SQL", time : "2023-12-30"},
-        {name : "Spring Boot整合JPA", link : "/pdf/Scattered/Spring Boot整合JPA.pdf", time : "2024-04-18"}
+        {name : "Spring Boot整合JPA", link : "/pdf/Scattered/Spring Boot整合JPA.pdf", time : "2024-04-18"},
+        {name : "SpringBoot整合MyBatis-Plus-IDEA", link : "/pdf/Scattered/SpringBoot整合MyBatis-Plus-IDEA.pdf", time : "2024-04-24"}
     ]
 }
