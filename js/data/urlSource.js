b45abb3dbbda53956dd86b6d31660b4d6a306973
yyym-y <yinmingyang89@gmail.com>
2024-04-27 23:50:41 +0800

    add url Source

---
+++ js/data/urlSource.js
@@ -0,0 +1,6 @@
+export default {
+    url : [
+        {name : "GO 语言中文文档", link : "https://www.topgoer.com/", time : "2024-04-26"},
+        {name : "amis 前端微框架中文文档", link : "https://aisuda.bce.baidu.com/amis/zh-CN/docs/index", time : "2024-04-26"},
+    ]
+}
