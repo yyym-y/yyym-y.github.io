f2bd2d7a024aaffd8b8948a8da38d3473f38909e
yyym-y <yinmingyang89@gmail.com>
2024-05-05 13:42:14 +0800

    add some note

--- js/data/scatterData.js
+++ js/data/scatterData.js
@@ -7,6 +7,15 @@ export default {
         {name : "SpringBoot整合MyBatis-Plus-IDEA", link : "/pdf/Scattered/SpringBoot整合MyBatis-Plus-IDEA.pdf", time : "2024-04-24"},
         {name : "Java泛型通配符 ? 快速手册", link: "/2023/10/12/Java泛型通配符", time : "2023-10-12"},
         {name : "JAVA 快速手册", link: "/2023/10/30/JAVA_SE", time : "2023-10-30"},
-        {name : "JavaWeb 快速手册", link: "/2023/10/30/JavaWeb", time : "2023-10-30"}
+        {name : "JavaWeb 快速手册", link: "/2023/10/30/JavaWeb", time : "2023-10-30"},
+        {name : "Linux 命令", link: "/2023/12/30/Linux命令", time : "2023-12-30"},
+        {name : "PHP 快速手册", link: "/2024/01/25/php", time : "2024-01-25"},
+        {name : "matplotlib.pyplot 手册", link: "/2023/12/30/matplotlib.pyplot", time : "2023-12-30"},
+        {name : "python各种鬼库", link: "/2023/12/30/python各种鬼库", time : "2023-12-30"},
+        {name : "python基础笔记", link: "/2023/12/30/python基础笔记", time : "2023-12-30"},
+        {name : "python进阶笔记", link: "/2023/12/30/python进阶笔记", time : "2023-12-30"},
+        {name : "Python 机器学习", link: "/2023/12/30/Python机器学习", time : "2023-12-30"},
+        {name : "贝叶斯分类器", link: "/2024/05/05/贝叶斯分类器", time : "2024-05-05"},
+        {name : "前端回忆录", link: "/2023/12/30/前端回忆录", time : "2023-12-30"},
     ]
 }
